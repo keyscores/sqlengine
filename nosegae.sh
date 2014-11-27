0bb85d9ea8fb52ffd87fd7dd16ebe3a5adaf933a
Jeremy Dillworth <jdillworth@gmail.com>
2014-11-26 22:13:20 -0500

    being smart about symbolic links (or less dumb)

--- nosegae.sh
+++ nosegae.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 GAEPATH=`which dev_appserver.py`
-GAEPATH=`echo $GAEPATH | sed 's/.*-> //'`
+
+if [ -L $GAEPATH ]; then
+    GAEPATH=`echo $GAEPATH | sed 's/.*-> //'`
+fi
+
 GAEPATH=`dirname $GAEPATH`
 
 PYTHONPATH=modules/ nosetests -w . --with-gae  --gae-lib-root=$GAEPATH $1 $2
